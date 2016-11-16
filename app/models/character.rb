38e488c3085e8270cb652fcc7f9e5e3cc347dd16
Raghu Betina <raghu@firstdraft.com>
2016-11-16 00:36:00 +0000

    Direct association between actors and characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,9 @@
 class Character < ApplicationRecord
   # Direct associations
 
+  belongs_to :actor,
+             :counter_cache => true
+
   belongs_to :movies,
              :counter_cache => true
 
